cbad56da25b6ff33e6045e32a7a5a742e21b0c79
LOCAL\a.vergnaud <a.vergnaud@gmail.com>
2025-08-13 10:39:48 +0200

    fix

--- src/main/java/com/example/bt_some_webapp/HelloController.java
+++ src/main/java/com/example/bt_some_webapp/HelloController.java
@@ -1,28 +1,29 @@
 package com.example.bt_some_webapp;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.core.JsonProcessingException;
 
 import some.MyBean;
-import some.MyUtility1;
-//import some.MyUtility2;
+//import some.MyUtility1;
+import some.MyUtility2;
 
 @RestController
 public class HelloController {
     
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String index() {
         MyBean myBean = new MyBean("Adr", 40);
-        
+/*        
         try {
             return MyUtility1.test(myBean);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             return "KO";
         }
-        
-        //return MyUtility2.test(myBean);
+ */        
+        return MyUtility2.test(myBean);
     }
 }
